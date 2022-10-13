1818b7c6ca303a44931cc9d3df17323e861cca4c
wenhui <WenHui_Xiao@everrich.com.tw>
2022-10-13 11:58:30 +0800

    git init

---
+++ DotNetCoreNLogDI/DotNetCoreNLogDI/App.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetCoreNLogDI
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Run()
+        {
+            _logger.LogInformation("LogInformation");
+            _logger.LogWarning("LogWarning");
+        }
+    }
+}
